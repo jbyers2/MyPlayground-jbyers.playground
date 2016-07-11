e3bb496c3dee282222ce54bc0d7f831aac876631
jbyers2 <jbyers2@gmail.com>
2016-07-11 15:04:45 -0500

    jbyers

---
+++ Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+"hello world"
+
+var myFirstVariable = 42
+
+let myFirstConstant = 77
+
+print(myFirstVariable, terminator:"")
+
+let s1:String = "Hello again"
+
+print(s1)
+
+print (s1,terminator:"")
+
+var vRight = "3"
+var vLeft = "<"
+
+print (vLeft + vRight,terminator:"")
+
+let names = ["<","3"]
+for n in names{
+    print(n)
+}
+
+
+let label="the answer is "
+let answer="234"
+print(label+answer,terminator:"")
+
+//ignore this
+
+print (s1)
+
+
+
